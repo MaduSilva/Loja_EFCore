35ee56a804ef98b58aa85c2d11018a455d75735e
FIC <FIC@info.local>
2020-09-08 16:46:50 -0300

    0809 finalizado

---
+++ Context/PedidoContext.cs
@@ -0,0 +1,25 @@
+﻿using EFCore.Domains;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.Context
+{
+    public class PedidoContext : DbContext
+    {
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DbSet<PedidoItem> PedidosItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-UTGRE9J\SQLEXPRESS;Initial Catalog=loja;Integrated Security=True");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
